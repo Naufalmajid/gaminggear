1c5b31d3f7919527d933573af7e5264eea965db6
Naufalmajid <86307904+Naufalmajid@users.noreply.github.com>
2021-06-23 11:14:06 +0800

    versi awal

---
+++ hardware.php
@@ -0,0 +1,47 @@
+<!--style sheet-->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+<div class="container">
+  <form action="proses/proses.shopnow.php" method="POST" class="row g-3 form-group">
+  <div class="container-fluid pt-3 pd-3">
+  <a href="index.php" class="navbar-brand">GAMINGGEAR</a>
+  
+  <form class="row g-3">
+  <div class="col-md-6">
+    <label for="inputEmail4" class="form-label">Email</label>
+    <input type="email" class="form-control" id="inputEmail4">
+  </div>
+
+  <div class="col-12">
+    <label for="inputAddress" class="form-label">Address</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+  </div>
+
+  <div class="col-md-6">
+    <label for="inputCity" class="form-label">City</label>
+    <input type="text" class="form-control" id="inputCity">
+  </div>
+  
+  <div class="col-md-4">
+    <label for="inputState" class="form-label">State</label>
+    <select id="inputState" class="form-select">
+      <option selected>Choose...</option>
+      <option>...</option>
+    </select>
+  </div>
+  <div class="col-md-2">
+    <label for="inputZip" class="form-label">Zip</label>
+    <input type="text" class="form-control" id="inputZip">
+  </div>
+  <div class="col-12">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gridCheck">
+      <label class="form-check-label" for="gridCheck">
+        Check me out
+      </label>
+    </div>
+  </div>
+  <div class="col-12">
+    <button type="submit" class="btn btn-primary">Sign in</button>
+  </div>
+</form>
---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> GAMING GEAR </title>
+
+    <!--style sheet-->
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/5a53d05429.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="style.css">
+
+    <!--Google font-->
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"> GAMING GEAR </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Location
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#">PC Budget</a></li>
+                  <li><a class="dropdown-item" href="#">Shop Now</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  
+                </ul>
+              </li>
+
+            </ul>
+            <form class="d-flex">
+             
+            </form>
+          </div>
+        </div>
+      </nav>
+      <!-- navbar atas close -->
+
+      <!-- seksyen 1 start -->
+      <section class="seksyen1">
+          <div class="container-fluld">
+              <div class="banner">
+                  <div class="banner-content">
+                      <h1 class="heading-Pey"><span class="tulisanPutih">GAMING GEAR</span></h1>
+                      <p class="tulisanPutih">DREAM IT . BUY IT . PLAY IT</p>
+                      <a href="pakagepc.php" class="btn btn-warning btn-lg">PAKAGE PC</a>
+                      <a href="hardware.php" class="btn btn-outline warning btn-lg">HARDWARE</a>
+                      
+
+                  </div>
+
+              </div>
+
+          </div>
+      </section>
+      <!-- seksyen 1 close -->
+
+      <!-- seksyen 2 start  -->
+      <section class="seksyen2">
+          <div class="container-fluid">
+              <h2>BENEFIT & ADVANTAGE</h2>
+              <BR>
+              <div class="row">
+                <div class="feature-box col-md-4">
+                    <i class="far fa-grin-beam icon fa-3x"></i>
+                 <h5>LATEST BUILD</h5>
+                 <P>LATEST ITEMS IN THE MARKET</P>
+                </div>
+                <div class="feature-box col-md-4">
+                    <i class="far fa-thumbs-up icon fa-3x"></i>
+                  <H5>WARRANTY</H5>
+                  <P>ALL OUR PRODUCT & HARDWARE</P>
+                </div>
+                <div class="feature-box col-md-4">
+                    <i class="fas fa-hand-paper icon fa-3x"></i>
+                    <H5>INSTALLMENT</H5>
+                    <P>WE BUILD & GIVE THE BEST AS POSSIBLE</P>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+      </section>
+      <!-- seksyen 2 close -->
+
+      <!-- seksyen 3 start -->
+
+      <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="pc1.png" class="d-block w-100" alt="" width="720" height="720">
+          </div>
+          <div class="carousel-item">
+            <img src="pc2.png" class="d-block w-100" alt="" width="720" height="720">
+          </div>
+          <div class="carousel-item">
+            <img src="pc3.png" class="d-block w-100" alt="" width="720" height="720">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+
+      <!-- seksyen 3 close -->
+      
+      <!-- footer -->
+      <section class= "footer">
+        <a href=""><i class= "fab fa-facebook-square"></i></a>
+        <a href=""></a>
+        <a href=""><i class= "fab fa-instagram-square"></i></a>
+        <a href=""></a>
+        <a href=""><i class= "fab fa-youtube-square"></i></a>
+        <a href=""></a>
+        <h6>@copyright GAMING GEAR 2021</h6>
+      </section>
+
+
+
+   
+</body>
+</html>
